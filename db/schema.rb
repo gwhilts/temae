0ef19e41caf821a2ee9f9718dde1848080babc60
Jerry Hilts <gwhilts@gmail.com>
2016-05-01 19:46:21 -0700

    Add User relation to Tasks
    - Task model: belongs_to :user
    - Gen'd migration AddUserIdToTasks
    - Ran migration: OK
    
    Closes #4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501231759) do
+ActiveRecord::Schema.define(version: 20160502024227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,9 +29,11 @@ ActiveRecord::Schema.define(version: 20160501231759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "context_id"
+    t.integer  "user_id"
   end
 
   add_index "tasks", ["context_id"], name: "index_tasks_on_context_id", using: :btree
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
