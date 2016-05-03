a72fd8efec73d7f6b06d3797811a422a1a44ed87
Jerry Hilts <gwhilts@gmail.com>
2016-05-02 22:22:40 -0700

    Clean up Gen'd Specs
    - Removed spec file for Context model. Will replace when ready to spec
      it out and start adding functionality beyond default ActiveRecord.
    - Added two pending specs to Task Model. Removed boiler plate.

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Task, type: :model do 
+  describe "#new" do
+    context "with no context provided" do
+      it "assigns task context to 'inbox'"
+    end
+    context "with no start date provided" do
+      it "assigns start to today's date"
+    end
+  end
 end
