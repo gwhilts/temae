4115eaa2fe2bce50326e49d27ebe1bd3fd62952f
Jerry Hilts <gwhilts@gmail.com>
2016-05-14 10:42:58 -0700

    Color codes Tasks in Task List by due date.
    Added #task_date_status helper:
    
    - Takes Rails flavored Date and returns:
      - "unspecified"
      - "past"
      - "today"
      - "future"
    
    Added class to task row in task_list helper using method above:
    
    - "due_unspecified"
    - "due_past"
    - "due_today"
    - "due_future"
    
    Added rules in CSS for:
    
    - task.row.due_past  -> reddish text
    - task.row.due_today -> yellowish test
    
    Closes #18

--- app/helpers/tasks_view_helper.rb
+++ app/helpers/tasks_view_helper.rb
@@ -1,5 +1,14 @@
 module TasksViewHelper
-  def foo
-    "<h1>foo</h1>"
+  def task_date_status(day)
+    case
+    when day == nil
+      "unspecified"
+    when day.today?
+      "today"
+    when day.past?
+      "past"
+    else
+      "future"
+    end
   end
 end
