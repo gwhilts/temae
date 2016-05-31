662f230fab8052d50c3b9eda5ba4c8f11028d4d6
Jerry Hilts <gwhilts@gmail.com>
2016-05-31 09:31:24 -0700

    Adds URL field to Cuke testing Tasks
    The tasks created background step definition
    'Fred has several tasks' did not contain any data
    for the URL field. This was causing a error when
    we tried to call `.empty` on nil objects.
    
    Added url data to each of the Tasks (incl. a
    few empty strings).
    
    All Features now green.

--- features/step_definitions/view_all_tasks_steps.rb
+++ features/step_definitions/view_all_tasks_steps.rb
@@ -18,36 +18,42 @@ Given(/^Fred has several tasks$/) do
   tasks = [{ name:       'Wash Car',
              start:      Time.utc(2016, 5, 7),
              due:        Time.utc(2016, 5, 9),
+             url:        'http://www.google.com',
              context:    user.contexts.third,
              user:       user
            },
            { name:       'Pick up library book',
              start:      Time.utc(2016, 5, 7),
              due:        Time.utc(2016, 5, 10),
+             url:        'http://www.google.com',
              context:    user.contexts.third,
              user:       user
            },
            { name:       'Return library book',
              start:      Time.utc(2016, 5, 11),
              due:        Time.utc(2016, 5, 24),
+             url:        'http://www.google.com',
              context:    user.contexts.second,
              user:       user
            },
            { name:       'Submit Pull Request',
              start:      Time.utc(2016, 5, 8),
              due:        Time.utc(2016, 5, 8),
+             url:        '',
              context:    user.contexts.second,
              user:       user
            },
            { name:       'Fix Issue #382',
              start:      Time.utc(2016, 5, 7),
              due:        Time.utc(2016, 5, 7),
+             url:        '',
              context:    user.contexts.first,
              user:       user
            },
            { name:       'Replace Furnace Filter',
              start:      Time.utc(2016, 5, 1),
              due:        Time.utc(2016, 5, 31),
+             url:        'http://www.google.com',
              context:    user.contexts.second,
              user:       user
            }
