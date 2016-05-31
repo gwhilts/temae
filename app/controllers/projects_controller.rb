a91f4ecf5b42115287dd991a7abcf45491f6ebd6
Jerry Hilts <gwhilts@gmail.com>
2016-05-31 09:17:49 -0700

    WIP: Created Projects Controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,49 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:edit, :update, :destroy]
+
+
+  # GET /projects
+  def index
+    # Fetch all @projects
+    # render view
+  end
+
+  # GET /projects/new
+  def new
+    # render :edit view w/ _form
+  end
+
+  # GET /projects/1/edit
+  # GET /projects/Name/edit
+  def edit
+    # render view w/ _form
+  end
+
+  # PUT /projects/1
+  # PUT /projects/Name
+  def update
+    # update record
+    # render index
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/Name
+  def destroy
+    # delete record
+    # render index
+  end
+
+  private
+  
+  def set_project
+    proj = params[:id]
+    case proj
+    when /^d{1,}$/
+      @project = Project.find proj
+    else
+      @project = Project.find_by_name proj 
+    end
+  end
+
+end
+
